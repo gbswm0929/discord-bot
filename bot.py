22fffd0de1b996e861ed458a84b0e42f62e0b0c9
gbswm0929 <gbswst0929@gmail.com>
2025-07-07 19:41:06 +0900

    commit

--- bot.py
+++ bot.py
@@ -235,7 +235,8 @@ async def select(interaction: discord.Interaction):
                     data = await response.json()
                     pretty = json.dumps(data, ensure_ascii=False, indent=2)
                     # print(pretty["mealServiceDietInfo"]["head"]["list_total_count"])
-                    print(data.get("mealServiceDietInfo"))
+                    a = data.get("mealServiceDietInfo")
+                    print(a.get("list_total_count"))
                     # if len(pretty) > 1900:
                     #     await interaction.response.send_message(f"```{pretty[:1900]}```")
                     # else:
